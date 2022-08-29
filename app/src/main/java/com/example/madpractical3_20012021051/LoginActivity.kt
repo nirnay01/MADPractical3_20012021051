02e93c58722f0ccd9ca1edd1b11267289e201944
tejasvi <pateltejasvi329@gmail.com>
2022-08-29 12:42:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/madpractical3_20012021051/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.example.madpractical3_20012021051
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.madpractical3_20012021051.databinding.ActivityLoginBinding
+
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    //paste here
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        //paste here
+    }
+}
